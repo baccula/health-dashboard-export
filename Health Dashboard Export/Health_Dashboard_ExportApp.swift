8bc0fac96edf1c4d808f13da83ef13b2088a5bb1
Mike Neuwirth <mike@neuwirth.cc>
2026-02-11 12:00:28 -0700

    Initial Commit

---
+++ Health Dashboard Export/Health_Dashboard_ExportApp.swift
@@ -0,0 +1,21 @@
+//
+//  Health_Dashboard_ExportApp.swift
+//  Health Dashboard Export
+//
+//  Created by Mike Neuwirth on 2/11/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct Health_Dashboard_ExportApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ Health Dashboard ExportTests/Health_Dashboard_ExportTests.swift
@@ -0,0 +1,17 @@
+//
+//  Health_Dashboard_ExportTests.swift
+//  Health Dashboard ExportTests
+//
+//  Created by Mike Neuwirth on 2/11/26.
+//
+
+import Testing
+@testable import Health_Dashboard_Export
+
+struct Health_Dashboard_ExportTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
